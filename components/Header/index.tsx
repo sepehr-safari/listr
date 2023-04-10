4a4d07565436bf27d05289adedeb2ee08f4fd3d6
Sepehr Safari <safari.sepehr@gmail.com>
2023-04-10 15:50:35 +0330

    add dynamic user ember

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,8 +1,6 @@
-import Link from 'next/link';
-
-import { UserIcon } from '@heroicons/react/24/outline';
-
 import { Avatar, Searchbar } from '@/components';
+
+import UserEmber from './UserEmber';
 
 export default function Header() {
   return (
@@ -23,26 +21,7 @@ export default function Header() {
             </div>
 
             <div className="flex w-1/4 justify-end gap-2">
-              <div className="dropdown-bottom dropdown-left dropdown">
-                <label tabIndex={0} className="btn-ghost btn-circle btn">
-                  <UserIcon width={24} />
-                </label>
-                <ul
-                  tabIndex={0}
-                  className="dropdown-content menu rounded-box w-32 bg-base-100 p-2 shadow-lg shadow-black"
-                >
-                  <li>
-                    <Link className="text-xs" href="/settings">
-                      Settings
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="text-xs" href="/logout">
-                      Logout
-                    </Link>
-                  </li>
-                </ul>
-              </div>
+              <UserEmber />
             </div>
           </div>
         </div>
