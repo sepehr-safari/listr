263a9d59e2c08e419dd998cb86e039255a53106a
Sepehr Safari <safari.sepehr@gmail.com>
2023-04-12 23:01:41 +0330

    add more relays

--- store/PoolSlice.ts
+++ store/PoolSlice.ts
@@ -14,7 +14,16 @@ const _pool = new SimplePool();
 
 const createPoolSlice: StateCreator<PoolSlice> = (_, get) => ({
   pool: {
-    relays: ['wss://relay.damus.io'],
+    relays: [
+      'wss://relay.damus.io',
+      'wss://relay.snort.social',
+      'wss://eden.nostr.land',
+      'wss://relay.nostr.info',
+      'wss://offchain.pub',
+      'wss://nostr-pub.wellorder.net',
+      'wss://nostr.fmt.wiz.biz',
+      'wss://nos.lol',
+    ],
     sub: (filters: Filter[]) => _pool.sub(get().pool.relays, filters),
     list: (filters: Filter[]) => _pool.list(get().pool.relays, filters),
     publish: (event: Event) => _pool.publish(get().pool.relays, event),
